18e5ab5e9a68789da71f0e6a856e3a679fdb7be7
anuvab-b <anuvabbiswas12@gmail.com>
2024-04-02 00:08:25 +0530

    Basic Widget Testing

---
+++ test/home_page_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tests/home_page.dart';
+
+void main() {
+  testWidgets(
+      "Given Counter is 0 when button is clicked then counter should be 1",
+      (widgetTester) async {
+    await widgetTester.pumpWidget(const MaterialApp(home: MyHomePage(title: "Counter App")));
+    final counter = find.text("0");
+    expect(counter, findsNWidgets(1));
+
+    final counter1 = find.text("1");
+    expect(counter1, findsNothing);
+
+    final incrementButton = find.byKey(const Key("increment_counter"));
+    await widgetTester.tap(incrementButton);
+
+    // setState(){}
+    await widgetTester.pump();
+
+    final counter2 = find.text("1");
+    expect(counter2, findsOneWidget);
+
+    final counter3 = find.text("0");
+    expect(counter3, findsNothing);
+    
+    expect(find.byType(AppBar), findsOneWidget);
+
+  });
+}
